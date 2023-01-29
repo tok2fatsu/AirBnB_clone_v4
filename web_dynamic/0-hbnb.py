65e97816acac1ac1ecf056f233ea7cc1b0e5abf4
tok2fatsu <mailit2moi@gmail.com>
2023-01-29 19:40:22 +0000

    Setup web dynamic

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,6 +4,8 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /hbnb: HBnB home page.
 """
+import uuid
+
 from flask import Flask, render_template
 
 from models import storage
@@ -16,25 +18,35 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def clode_db(error):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/0-hbnb", strict_slashes=False)
 def hbnb():
     """Displays the main HBnB filters HTML page."""
     states = list(storage.all(State).values())
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)
+
     amenities = list(storage.all("Amenity").values())
+    amenities = sorted(amenities, key=lambda k: k.name)
+
     places = list(storage.all("Place").values())
+    places = sorted(places, key=lambda k: k.name)
+
     return render_template(
         "0-hbnb.html",
-        states=states,
+        states=st_ct,
         amenities=amenities,
         places=places,
     )
 
 
 if __name__ == "__main__":
-    storage.reload()
-    app.run(host="0.0.0.0", 5000)
+    """ Main Fun """
+    app.run()
